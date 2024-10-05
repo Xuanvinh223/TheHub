c18426bf67b9d759ad713e487a66392cb593002d
xuanvinh223 <phamxuanvinh020203@gmail.com>
2024-10-05 16:28:31 +0700

    ok setup src

--- module/angularjs/controller/index.js
+++ module/angularjs/controller/index.js
@@ -1,7 +1,8 @@
 import { database, ref, set, push, remove, onValue } from 'https://xuanvinh223.github.io/TheHub/module/firebase.js';
 
+
 function indexController($scope) {
-    $scope.message = "Hello from indexController!";
+    $scope.message = "Hello world!";
     console.log(database); // Giả sử biến `database` có sẵn trong môi trường
 }
 
