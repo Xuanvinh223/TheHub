4a90adbca1eab09f9235f0893eb5f661f431e8c0
xuanvinh223 <phamxuanvinh020203@gmail.com>
2024-10-05 14:32:09 +0700

    reup

--- module/angularjs/main.js
+++ module/angularjs/main.js
@@ -1,8 +1,9 @@
-import { database, ref, set, push, remove, onValue } from '/module/firebases.js';
+import { database, ref, set, push, remove, onValue } from '/module/firebase.js';
 
 const app = angular.module('myApp', []);
 
 app.controller("myController",myController)
 function myController() {
+    console.log(database);
     
 }
