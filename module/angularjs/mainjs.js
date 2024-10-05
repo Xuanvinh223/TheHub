f2bafac03647c5c78279948b4e1c6d956a39371f
xuanvinh223 <phamxuanvinh020203@gmail.com>
2024-10-05 15:08:37 +0700

    reup

--- module/angularjs/mainjs.js
+++ module/angularjs/mainjs.js
@@ -1,4 +1,4 @@
-import { database, ref, set, push, remove, onValue } from '/module/firebase.js';
+import { database, ref, set, push, remove, onValue } from 'https://xuanvinh223.github.io/TheHub/module/firebase.js';
 
 const app = angular.module('myApp', []);
 
